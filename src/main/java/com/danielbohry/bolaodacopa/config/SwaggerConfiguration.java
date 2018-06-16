938f7b4f5904301058f69c22350e9ecdd25e5841
Daniel Bohry <danielbohry@gmail.com>
2018-06-16 12:45:16 -0300

    removendo swagger

--- src/main/java/com/danielbohry/bolaodacopa/config/SwaggerConfiguration.java
+++ src/main/java/com/danielbohry/bolaodacopa/config/SwaggerConfiguration.java
@@ -5,12 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
-@EnableSwagger2
 class SwaggerConfiguration {
 
     @Bean
